108d3c346b2728a588dba1ac1ef9b8eaaa3c5723
Jorge Reis <jorgew.reis@outlook.com>
2026-09-25 16:59:42 -0300

    Remove duplicate profile link from settings

--- resources/views/portal/fokus-law.blade.php
+++ resources/views/portal/fokus-law.blade.php
@@ -109,7 +109,7 @@
   </template>
   <script src="/assets/js/fokus.min.js?v=20260916-fokus-styles-2.7.0"></script>
   <script src="/assets/js/api-client.js"></script>
-  <script src="/portal/assets/fokus-law-shell.js?v=20260925-shell-v6" defer></script>
+  <script src="/portal/assets/fokus-law-shell.js?v=20260925-shell-v7" defer></script>
   <script src="/assets/js/portal-profile.js?v=20260925-profile-v4" defer></script>
 </body>
 </html>
